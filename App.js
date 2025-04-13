7debe49cb08e5c64c81fc2193013ff7dcda36a00
Gabriel-Sanchez <gabriel.g.sanchez.c@gmail.com>
2025-04-13 17:57:32 -0500

    main

--- App.js
+++ App.js
@@ -1,11 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
+import * as Location from 'expo-location';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import * as React from 'react';
+import { Marker, Polyline } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import {GOOGLE_MAPS_KEY} from '@env'
+import ContadorVelocidad from './componets/ContadorVelocidad';
 
 export default function App() {
+
+  const [origin, setOrigin] = React.useState(
+    {
+      latitude: 33.640411,
+      longitude: -84.419853,
+    }
+  );
+  const [destination, setDestination] = React.useState(
+    {
+      latitude: 33.753746,
+      longitude: -84.386330,
+    }
+  );
+
+  React.useEffect(()=>{
+    getLocationPermission();
+  }, [])
+
+  async function getLocationPermission(){
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted' ){
+      alert('permission denied')
+    }
+    let location = await Location.getCurrentPositionAsync({})
+    const current = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude
+    }
+    setOrigin(current)
+
+  }
+
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mapas Geo</Text>
       <StatusBar style="auto" />
+      <MapView style={styles.map} 
+      initialRegion={{latitude: origin.latitude, 
+      longitude: origin.longitude, 
+    latitudeDelta: 0.09,
+  longitudeDelta: 0.04}}
+      
+      >
+        <Marker draggable
+        coordinate={origin}
+        onDragEnd={(direction) => setOrigin(direction.nativeEvent.coordinate)  } />
+
+        <Marker draggable
+        coordinate={destination}
+        onDragEnd={(direction) => setDestination(direction.nativeEvent.coordinate)  } />
+
+        <MapViewDirections origin={origin}
+        destination={destination}
+        apikey={GOOGLE_MAPS_KEY}/>
+
+        <Polyline coordinates={[origin, destination]}
+        strokeColor='green'
+        strokeWidth={8}
+        >
+
+        </Polyline>
+      </MapView>
+
+      <ContadorVelocidad />
+
     </View>
   );
 }
@@ -17,4 +86,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: "100%",
+    height: "90%"
+  }
 });
